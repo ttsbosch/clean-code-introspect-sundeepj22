f16b01478b8c25fec61bd0a8a08f52da05058489
sundeepj22 <100696391+sundeepj22@users.noreply.github.com>
2024-06-27 16:33:31 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,6 +1,23 @@
 #include "StringCalculator.h"
+#include <vector>
+#include <cctype>
+#include <numeric> // for std::accumulate
 
 int StringCalculator::add(std::string numbers)
 {
-  return 0;
+    std::vector<std::int32_t> digit_list;
+    std::int32_t sumOfDigitList{std::accumulate(digit_list.begin(), digit_list.end(), 0)};
+    return sumOfDigitList;
+}
+
+void StringCalculator::ExtractDigits(const std::string& input, std::vector<std::int32_t>& digit_list)
+{
+      digit_list.clear();
+      for (char c : input)
+      {
+          if (std::isdigit(c))
+          {
+              digit_list.push_back(c - '0');  // Convert char to integer and add to digit_list
+          }
+      }
 }
